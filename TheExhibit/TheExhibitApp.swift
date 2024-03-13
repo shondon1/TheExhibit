1af288ee0df0be9d06b871af2587395badfb53c4
Rashon <rashyslop@outlook.com>
2024-03-12 18:16:45 -0600

    Initial Commit

---
+++ TheExhibit/TheExhibitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheExhibitApp.swift
+//  TheExhibit
+//
+//  Created by Rashon Hyslop on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TheExhibitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
